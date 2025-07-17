a3f69dde6c289d30e4320f6e198f15783946dddc
Lxg1c <teran_2005@mail.ru>
2025-07-17 22:13:31 +0300

    fixed

--- src/enteties/profile/types.ts
+++ src/enteties/profile/types.ts
@@ -6,19 +6,19 @@ export type ProfileData = {
         nickname: string;
         avatar_url: string;
         age: number;
-    };
-    city: {
-        id: string;
-        name: string;
-    };
-    is_private: boolean;
-    telegram_id: string;
-    about: string;
-    points: number;
-    calling_limit: number;
-    going_limit: number;
-    account_status: string;
-    last_login_at: string;
-    created_at: string;
-    updated_at: string;
+        city: {
+            id: string;
+            name: string;
+        };
+        is_private: boolean;
+        telegram: string;
+        about: string;
+        points: number;
+        calling_limit: number;
+        going_limit: number;
+        account_status: string;
+        last_login_at: string;
+        created_at: string;
+        updated_at: string;
+    }
 };
