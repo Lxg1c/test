f5fd4b8f1323abc79a159a4c5e6fd27bbb4157a6
Lxg1c <teran_2005@mail.ru>
2025-07-17 22:01:27 +0300

    add validator

--- src/enteties/profile/api.ts
+++ src/enteties/profile/api.ts
@@ -1,41 +1,36 @@
-// api.ts
 import axios from "axios";
-import { z } from "zod";
+import { ProfileData } from "@/enteties/profile/types";
+import {z} from "zod";
 
-// Опишем схему для profile
 const ProfileDataSchema = z.object({
     data: z.object({
+        about: z.string(),
+        account_status: z.string(),
+        age: z.number(),
+        avatar_url: z.string(),
+        calling_limit: z.number(),
+        city: z.object({
+            id: z.string(),
+            name: z.string(),
+        }),
+        created_at: z.string(),
+        going_limit: z.number(),
         id: z.string(),
-        role: z.string(),
+        last_login_at: z.string(),
         name: z.string(),
         nickname: z.string(),
-        avatar_url: z.string(),
-        age: z.number(),
-    }),
-    city: z.object({
-        id: z.string(),
-        name: z.string(),
-    }),
-    is_private: z.boolean(),
-    telegram_id: z.string(),
-    about: z.string(),
-    points: z.number(),
-    calling_limit: z.number(),
-    going_limit: z.number(),
-    account_status: z.string(),
-    last_login_at: z.string(),
-    created_at: z.string(),
-    updated_at: z.string(),
+        points: z.number(),
+        role: z.string(),
+        telegram: z.string(),
+        updated_at: z.string(),
+    })
 });
 
-// Тип из схемы (опционально, если хочешь, чтобы ts автоматически подхватил)
-export type ProfileData = z.infer<typeof ProfileDataSchema>;
 
 export const getProfileInfo = async (): Promise<ProfileData | null> => {
     try {
         const { data } = await axios.get("https://igroom.ru/api/v2/profile/5e800be0-088e-41cb-b549-10ebf4a13591");
 
-        // Валидация
         const parsedData = ProfileDataSchema.safeParse(data);
 
         if (!parsedData.success) {
@@ -43,7 +38,7 @@ export const getProfileInfo = async (): Promise<ProfileData | null> => {
             return null;
         }
 
-        return parsedData.data;
+        return data;
     } catch (e) {
         console.error(e);
         return null;
